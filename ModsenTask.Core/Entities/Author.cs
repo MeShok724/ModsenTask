fd4a24bdd951227594609b04b5dca8f26d9363cf
Mikhail Kapytok <mishakopytok@gmail.com>
2025-01-29 17:59:41 +0300

    Add project files.

---
+++ ModsenTask.Core/Entities/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModsenTask.Core.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string Country { get; set; } = string.Empty;
+
+        public List<Book> Books { get; set; } = new();
+    }
+}
---
+++ ModsenTask.Core/Entities/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModsenTask.Core.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string ISBN { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int AuthorId { get; set; }
+        public DateTime? BorrowedAt { get; set; }
+        public DateTime? ReturnBy { get; set; }
+
+        public Author Author { get; set; } = null!;
+    }
+}
